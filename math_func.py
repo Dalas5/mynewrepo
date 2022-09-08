78814aa86b7f0ee651ca11b6278e9bc279868ea9
Diego Alas <dalas7610@gmail.com>
2022-09-08 15:43:50 -0600

    math_func.py resta() developed

--- math_func.py
+++ math_func.py
@@ -2,15 +2,19 @@ def suma():
     pass
 
 
-def resta():
-    pass
+def resta(n1, n2):
+    # Esta funcion resta
+    # dos numeros como parametros
+    # retorna la resta del primer parametro menos el segundo
+    result = n1 - n2
+    return result
+
 
 def run():
     print("Ingrese dos numeros")
-    num1 = input("Ingrese primer número:")
-    num2 = input("Ingrese segundo número:")
-    print(f"La suma es: {suma(num1, num2)} y la resta es: {resta(num1, num2)})    
-    
+    num1 = int(input("Ingrese primer número:"))
+    num2 = int(input("Ingrese segundo número:"))
+    print(f"La suma es: {suma(num1, num2)} y la resta es: {resta(num1, num2)}")
 
 
 if __name__ == '__main__':
