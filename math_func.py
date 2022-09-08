d2b10fca1f6a3ead5409f109d0a6253fcd95099a
Diego Alas <88285684+Dalas5@users.noreply.github.com>
2022-09-08 15:34:58 -0600

    Update math_func.py

--- math_func.py
+++ math_func.py
@@ -1,5 +1,17 @@
+def suma():
+    pass
+
+
+def resta():
+    pass
+
+
 def run():
-    print("Hello world!")
+    print("Ingrese dos numeros")
+    num1 = input("Ingrese primer número:")
+    num2 = input("Ingrese segundo número:")
+    print(f"La suma es: {suma(num1, num2)} y la resta es: {resta(num1, num2)})    
+    
 
 
 if __name__ == '__main__':
